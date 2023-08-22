1201aef2ae2150a5d4075eab1bbe38931714a3d0
AmirShamsNIH <shamsaddinisha@nih.gov>
2023-08-22 10:13:42 -0400

    fix test scenario

--- test/test_scenarios.sh
+++ test/test_scenarios.sh
@@ -6,7 +6,7 @@ sbatch --cpus-per-task=16 --mem=16g --time=24:00:00
 #GEX
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++
 #test fastq input
-/data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
+python /data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
 --title sc_gex_fastq \
 --input /data/RTB_GRS/ActiveProjects/GRS_0088_Wishart/cellranger_mkfastq_0204_AHLYKTBGXM/HLYKTBGXM/ \
 --input-type fastq \
@@ -17,7 +17,7 @@ sbatch --cpus-per-task=16 --mem=16g --time=24:00:00
 # ----------------------------------------------------
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-/data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
+python /data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
 --title sc_gex_bcl \
 --input /data/RTB_GRS/SequencerRuns/GRS_0088_Wishart/230324_NS500353_0204_AHLYKTBGXM/ \
 --input-type bcl \
@@ -32,7 +32,7 @@ sbatch --cpus-per-task=16 --mem=16g --time=24:00:00
 #VDJ
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-/data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
+python /data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
 --title sc_vdj_bcl \
 --input /data/RTB_GRS/SequencerRuns/GRS_0098_Lu/230407_NS500353_0205_AHM2KKBGXM/ \
 --input-type bcl \
@@ -42,7 +42,7 @@ sbatch --cpus-per-task=16 --mem=16g --time=24:00:00
 --sample-sheet /data/RTB_GRS/ActiveProjects/GRS_0098_Lu/cellranger_mkfastq_0205_AHM2KKBGXM/CellRanger_SampleSheet_GRS_0098_corrected.csv \
 --mode slurm
 
-/data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
+python /data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
 --title sc_vdj_fastq \
 --input /data/RTB_GRS/ActiveProjects/GRS_0098_Lu/cellranger_mkfastq_0205_AHM2KKBGXM/HM2KKBGXM/ \
 --input-type fastq \
@@ -57,7 +57,7 @@ sbatch --cpus-per-task=16 --mem=16g --time=24:00:00
 #FBC
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #FBC WITH MULTI RUN
-/data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
+python /data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
 --title sc_fbc_fastq \
 --input /data/RTB_GRS/ActiveProjects/GRS_0112_Panda/cellranger_mkfastq_0210_AHLYYGBGXM/HLYYGBGXM /data/RTB_GRS/ActiveProjects/GRS_0112_Panda/cellranger_mkfastq_39_AAC3F27HV/AAC3F27HV /data/RTB_GRS/ActiveProjects/GRS_0112_Panda/cellranger_mkfastq_50_AAC3HKGHV/AAC3HKGHV \
 --input-type fastq \
@@ -68,7 +68,7 @@ sbatch --cpus-per-task=16 --mem=16g --time=24:00:00
 # -------------------------------------------------------
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-/data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
+python /data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
 --title sc_fbc_bcl \
 --input /data/RTB_GRS/SequencerRuns/GRS_0112_Panda/230606_VH00286_39_AAC3F27HV/ /data/RTB_GRS/SequencerRuns/GRS_0112_Panda/230510_NS500353_0210_AHLYYGBGXM/ /data/RTB_GRS/SequencerRuns/GRS_0112_Panda/230710_VH00286_50_AAC3HKGHV/ \
 --input-type bcl \
@@ -81,7 +81,7 @@ sbatch --cpus-per-task=16 --mem=16g --time=24:00:00
 
 ######################
 #HTO
-/data/$USER/Development/10Xsnakepipe/grs_sc10x.py run \
+python /data/$USER/Development/10Xsnakepipe/grs_sc10x.py run \
 --title sc_hto_fastq \
 --input /data/RTB_GRS/ActiveProjects/GRS_0168_Zheng/cellranger_mkfastq_0212_AHFLJGBGXC/HFLJGBGXC/ /data/RTB_GRS/ActiveProjects/GRS_0168_Zheng/cellranger_mkfastq_0204_AHFKCFBGXC/HFKCFBGXC/ \
 --input-type fastq \
@@ -90,7 +90,7 @@ sbatch --cpus-per-task=16 --mem=16g --time=24:00:00
 --output /data/$USER/Test_Space/GRS_SC10X \
 --mode slurm
 
-/data/$USER/Development/10Xsnakepipe/grs_sc10x.py run \
+python /data/$USER/Development/10Xsnakepipe/grs_sc10x.py run \
 --title sc_hto_fastq \
 --input /data/RTB_GRS/ActiveProjects/GRS_0168_Zheng/cellranger_mkfastq_0212_AHFLJGBGXC/HFLJGBGXC/ /data/RTB_GRS/ActiveProjects/GRS_0168_Zheng/cellranger_mkfastq_0204_AHFKCFBGXC/HFKCFBGXC/ \
 --input-type bcl \
@@ -105,7 +105,7 @@ sbatch --cpus-per-task=16 --mem=16g --time=24:00:00
 ######################
 #ATAC
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-/data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
+python /data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
 --title sc_atac_fastq \
 --input /data/RTB_GRS/ActiveProjects/NGS_0543_Singh/cellranger_atac_mkfastq_0190/HF3MLBGXK/ /data/RTB_GRS/ActiveProjects/NGS_0543_Singh/cellranger_atac_mkfastq_0188/HC2HKBGXK/ /data/RTB_GRS/ActiveProjects/NGS_0543_Singh/cellranger_atac_mkfastq_0177/HF3KFBGXK/ \
 --input-type fastq \
@@ -115,7 +115,7 @@ sbatch --cpus-per-task=16 --mem=16g --time=24:00:00
 --mode slurm
 # -------------------------------------------------------
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-/data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
+python /data/$USER/Development/GRS_SC10X/grs_sc10x.py run \
 --title sc_atac_bcl \
 --input /data/RTB_GRS/SequencerRuns/NGS_0543_singh/220420_NS500189_0188_AHC2HKBGXK/ /data/RTB_GRS/SequencerRuns/NGS_0543_singh/220607_NS500189_0190_AHF3MLBGXK/ /data/RTB_GRS/SequencerRuns/NGS_0543_singh/220608_NS500353_0177_AHF3KFBGXK/ \
 --input-type bcl \
